31b5d9fc67cd80f621fbeb8b4838752eda7b22d3
Luke Clark <lukeclarksfo@gmail.com>
2024-07-11 10:32:01 +0200

    Update

--- linkedlist.c
+++ linkedlist.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 struct node
@@ -25,5 +26,5 @@ struct node *insertafter(int v, struct node *t)
 
 main()
 {
-    
+
 }
---
+++ stack.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static struct node
+{int key; struct node *next;};
+
+static struct node *head, *z, *t;
+
+stackinit()
+{
+    head = (struct node *) malloc(sizeof *head);
+    z = (struct node *) malloc(sizeof *z);
+    head->next = z; head->key=0;
+    z->next = z;
+}
+push(int v)
+{
+    t = (struct node *) malloc(sizeof *t);
+    t->key = v; t->next = head->next;
+    head->next = t;
+}
+int pop()
+{
+    int x;
+    t = head->next; head->next = t->next;
+    x = t->key;
+    free(t);
+    return x;
+}
+int stackempty()
+{ return head->next == z;}
+
+main()
+{
+    stackinit();
+    
+    push(5);
+    push(9);
+    push(8);
+    push(pop()+pop());
+    push(4);
+    push(6);
+    push(pop()*pop());
+    push(pop()*pop());
+    push(7);
+    push(pop()+pop());
+    push(pop()*pop());
+    printf("Result on stack is %d\n", pop());
+}
